0883de32dca7320015019e2babdb20a5890bc09c
Iheb Chaoued <ihebchaoued@gmail.com>
2024-04-23 15:34:03 +0000

    multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,11 @@ import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((photoResponse, userResponse) => {
+    .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
-    .catch(() => {
+    .catch((error) => {
+      console.error('Error occurred during signup:', error);
       console.log('Signup system offline');
     });
 }
