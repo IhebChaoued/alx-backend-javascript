d96150d76fe3818cbec8dc3d60bdb8157fbad614
Iheb Chaoued <ihebchaoued@gmail.com>
2024-04-25 23:20:40 +0000

    Fixed

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,19 @@
 export default class Building {
   constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error();
     this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
     return this._sqft;
   }
 
+  set sqft(value) {
+    this._sqft = value;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate the ${this.constructor.name}!`;
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
