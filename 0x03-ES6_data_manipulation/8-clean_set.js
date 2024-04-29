88f2e36aaf69f96320802b600fc5f0f43e29d36b
Iheb Chaoued <ihebchaoued@gmail.com>
2024-04-29 20:19:12 +0000

    it's always about the strings

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ function cleanSet(set, startString) {
   const result = [];
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+      result.push(value);
     }
   });
   return result.join('-');
