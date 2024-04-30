a87ccbb78745601b9fe0a1612b49f5cd0e0a22cb
Iheb Chaoued <ihebchaoued@gmail.com>
2024-04-30 22:05:37 +0000

    Weak..

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,19 +3,18 @@ const weakMap = new WeakMap();
 export { weakMap };
 
 function queryAPI(endpoint) {
-  if (weakMap.has(endpoint)) {
-    const count = weakMap.get(endpoint) + 1;
-    weakMap.set(endpoint, count);
-
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-
-    return count;
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
   }
 
-  weakMap.set(endpoint, 1);
-  return 1;
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  return count;
 }
 
 export default queryAPI;
