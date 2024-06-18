2efeeac77da15ff8d93549078f6b77025dab2b2a
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-18 21:43:56 +0000

    Counting

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,42 @@
 const fs = require('fs');
 
-function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf8');
-        const lines = data.split('\n').filter(line => line.trim() !== '').slice(1);
-        const students = {};
-        let totalStudents = 0;
-
-        lines.forEach(line => {
-            const [firstname, lastname, age, field] = line.split(',');
-            if (!students[field]) {
-                students[field] = [];
-            }
-            students[field].push(firstname);
-            totalStudents += 1;
-        });
-
-        console.log(`Number of students: ${totalStudents}`);
-        for (const field in students) {
-            console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+const countStudents = (path) => {
+  try {
+    const data = fs.readFileSync(path, 'utf-8').split('\n').splice(1);
+    let dataLen = 0;
+    const fieldObj = {};
+    if (data) {
+      data.forEach((e) => {
+        if (e.length) {
+          const line = e.split(',');
+          const lineLen = line.length;
+          dataLen += 1;
+          const filed = line[lineLen - 1];
+          const firstName = line[0];
+          if (filed in fieldObj) {
+            fieldObj[filed].data.push(firstName);
+            fieldObj[filed].count += 1;
+          } else {
+            fieldObj[filed] = {
+              data: [firstName],
+              count: 1,
+            };
+          }
         }
-    } catch (error) {
-        throw new Error('Cannot load the database');
+      });
     }
-}
+
+    console.log(`Number of students: ${dataLen}`);
+    for (const field in fieldObj) {
+      if (field in fieldObj) {
+        const data = fieldObj[field].data.join(', ');
+        const fLen = fieldObj[field].count;
+        console.log(`Number of students in ${field}: ${fLen}. List: ${data}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
 
 module.exports = countStudents;
