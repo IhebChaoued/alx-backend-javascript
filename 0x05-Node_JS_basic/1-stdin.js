f9cac161deccc3370851b1a62faac2d06f655184
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-18 21:42:23 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const line = process.stdin.read();
+  if (line) process.stdout.write(`Your name is: ${line}`);
 });
 
-process.on('exit', () => {
-    console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
