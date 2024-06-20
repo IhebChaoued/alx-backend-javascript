f61ef7067ed5febb6722cf3dfb973b5b8b0237f6
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-20 18:27:14 +0000

    Stubs

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,14 +1,11 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  // Stub Utils.calculateNumber to always return 10
   const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
-  // Call the stubbed function
   const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${sum}`);
 
-  // Restore the stub after testing
   calculateNumberStub.restore();
 }
 
