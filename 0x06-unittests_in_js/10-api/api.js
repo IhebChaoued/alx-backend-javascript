685b1c82fd56d19dfe387d11f9b9cdad471120f9
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-21 12:09:10 +0000

    Deep & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,26 +1,40 @@
 const express = require('express');
+
 const app = express();
-const bodyParser = require('body-parser');
+const port = 7865;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get('/available_payments', (req, res) => {
-  res.json({
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (request, response) => {
+  response.send(`Payment methods for cart ${request.params.id}`);
+});
+
+app.get('/available_payments', (request, response) => {
+  response.set('Content-Type', 'application/json');
+  const payMethods = {
     payment_methods: {
       credit_cards: true,
       paypal: false
     }
-  });
+  };
+  response.send(payMethods);
 });
 
-app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+app.post('/login', (request, response) => {
+  const userName = request.body.userName;
+  if (userName) {
+    response.send(`Welcome ${userName}`);
+  } else {
+    response.status(404).send();
+  }
 });
 
-const PORT = 7865;
-const server = app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
 });
 
-module.exports = server;
+module.exports = app;
